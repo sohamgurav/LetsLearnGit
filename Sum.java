6adf586fc64c5b3f1cf9a4f6f6cce865d801cf96
sohamg18 <guravsoham18@gmail.com>
2021-07-22 12:12:12 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,6 @@
+public class Sum{
+        public static void main(String args[]){
+            int num1=10, num2=20, sum;
+            sum=num1+num2
+            System.out.println("Sum of these numbers :"+sum);
+}
